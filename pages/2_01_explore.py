205fb1b6ac3ddfdb987f6b4857648b221b16d687
dfrome <dfrome@users.noreply.github.com>
2025-04-09 18:16:59 +0200

    updt

--- pages/2_01_explore.py
+++ pages/2_01_explore.py
@@ -5,22 +5,21 @@ from PIL import Image
 
 st.set_page_config(page_title="Plotting Demo", page_icon="📈")
 
-st.markdown("# CO2 Demo")
-st.sidebar.header("CO2 Demo")
+st.markdown("# CO2 Exploration des données")
+st.sidebar.header("Nous explorons le données")
 st.write(
-    """Nous explorons les données. Elles sont très complètes.  
-    Il y a toutefois beaucoup d'erreurs manifestes de saisie."""
+    """Nous explorons tout d'abord les données."""
 )
-
-image_path = "images/vehiclesByCty_01.png"
-image = Image.open(image_path)
-st.image(image, caption="Distribution des véhicules par pays.")
-st.write("""
-         """)
 
 image_path = "images/vehiclesByFtByCty_01.png"
 image = Image.open(image_path)
-st.image(image, caption="Energie des véhicules par pays.")
+st.image(image, caption="Type de carburant des véhicules par pays.")
+st.write("""
+         """)
+
+image_path = "images/CO2ByFt.png"
+image = Image.open(image_path)
+st.image(image, caption="Comparaison des émissiones de CO2 par type de carburant.")
 st.write("""
          """)
 
@@ -28,6 +27,12 @@ image_path = "images/CorrectionsEmpattement.jpg"
 image = Image.open(image_path)
 st.image(image, caption="Outliers d'empattements comparés à la masse.")
 
+st.write("""Les donnés sont très complètes.  
+    Il y a toutefois beaucoup d'erreurs manifestes de saisie.   
+    Supprimons les données inutilisables, traitons les aberrations et interprêtons les codes.     """)
+
+
+"""
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
@@ -42,6 +47,7 @@ for i in range(1, 11):
     time.sleep(0.05)
 
 progress_bar.empty()
+"""
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
