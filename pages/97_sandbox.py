f765c0b346f2a51ffcd8d5f5ff2cd7e94eb6ee37
dfrome <dfrome@users.noreply.github.com>
2025-05-06 11:47:44 +0200

    tab

--- pages/97_sandbox.py
+++ pages/97_sandbox.py
@@ -9,39 +9,38 @@ st.subheader("mon subheader")
 st.sidebar.write("ma description")
 st.markdown(f'##### go go go')
 
-with st.echo(code_location='below'):
-    if 'A3' not in st.session_state:
-        st.session_state.A3 = 5
-    if 'B3' not in st.session_state:
-        st.session_state.B3 = 7
+if 'A3' not in st.session_state:
+    st.session_state.A3 = 5
+if 'B3' not in st.session_state:
+    st.session_state.B3 = 7
 
-    def _set_num_A3_cb():
-        st.session_state.A3 = st.session_state.num_A3
-    def _set_num_B3_cb():
-        st.session_state.B3 = st.session_state.num_B3
+def _set_num_A3_cb():
+    st.session_state.A3 = st.session_state.num_A3
+def _set_num_B3_cb():
+    st.session_state.B3 = st.session_state.num_B3
 
-    radio = st.radio(label="", label_visibility="hidden", options=["Set A3", "Set B3", "Add them"], horizontal=True)
+radio = st.radio(label="", label_visibility="hidden", options=["Set A3", "Set B3", "Add them"], horizontal=True)
 
-    if radio == "Set A3":
-        st.session_state.A3 = st.number_input(
-            label="What is A3?",
-            min_value=0, max_value=100,
-            value=st.session_state.A3,
-            on_change=_set_num_A3_cb,
-            key='num_A3'
-        )
-        st.write(f"You set A3 to {st.session_state.A3}")
-    elif radio == "Set B3":
-        st.session_state.B3 = st.number_input(
-            label="What is B3?",
-            min_value=0, max_value=100,
-            value=st.session_state.B3,
-            on_change=_set_num_B3_cb,
-            key='num_B3'
-        )
-        st.write(f"You set B3 to {st.session_state.B3}")
-    elif radio == "Add them":
-        st.write(f"A3 = {st.session_state.A3} and B3 = {st.session_state.B3}")
-        button = st.button("Add A3 and B3")
-        if button:
-            st.write(f"A3 + B3 = {st.session_state.A3*10 + st.session_state.B3}")
+if radio == "Set A3":
+    st.session_state.A3 = st.number_input(
+        label="What is A3?",
+        min_value=0, max_value=100,
+        value=st.session_state.A3,
+        on_change=_set_num_A3_cb,
+        key='num_A3'
+    )
+    st.write(f"You set A3 to {st.session_state.A3}")
+elif radio == "Set B3":
+    st.session_state.B3 = st.number_input(
+        label="What is B3?",
+        min_value=0, max_value=100,
+        value=st.session_state.B3,
+        on_change=_set_num_B3_cb,
+        key='num_B3'
+    )
+    st.write(f"You set B3 to {st.session_state.B3}")
+elif radio == "Add them":
+    st.write(f"A3 = {st.session_state.A3} and B3 = {st.session_state.B3}")
+    button = st.button("Add A3 and B3")
+    if button:
+        st.write(f"A3 + B3 = {st.session_state.A3*10 + st.session_state.B3}")
